4a0e2e1dd527e8d0414c936be992f3aac345b271
enver1 <enver.flores.2036@gmail.com>
2023-12-08 14:56:25 -0500

    Mostrar variable en footer
    Mostrar anio
     this.anio = new Date().getFullYear();

--- holaMundo/src/app/components/footer/footer.component.ts
+++ holaMundo/src/app/components/footer/footer.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  anio: number;
+  constructor(){
+    this.anio = new Date().getFullYear();
+  }
 }
